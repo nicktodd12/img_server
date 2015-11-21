1225104183e5abe64faf5eb33699ac856a21f1fd
JD Porterfield <jdvporterfield@gmail.com>
2015-11-20 22:21:29 -0600

    hw6

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cookieParser())
 
 //CORS
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "//jdnet.herokuapp.com");
+	res.header("Access-Control-Allow-Origin", "jdnet.herokuapp.com");
  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Credentials", true);
 	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
